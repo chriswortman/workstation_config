1b86029b1960b059722010c7386d5240e8bf77ee
chriswortman <chriswortman@gmail.com>
2020-06-21 15:09:01 -0700

    updating for ubuntu 20.04/pop-os 20.04

--- wks_bootstrap.sh
+++ wks_bootstrap.sh
@@ -4,6 +4,6 @@
 ## tested with ubuntu 16.04
 
 apt update
-apt install -y git ansible 
-pip install --upgrade setuptools
+apt install -y git ansible python3 python3-pip
+pip3 install --upgrade setuptools
 
